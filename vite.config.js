d3beeddf8405f894a06d6e728dd48117ceb89198
Daniel Tirado <daniel.tirado@estudiante.uam.es>
2026-04-23 16:56:40 +0200

    build: configurar Vite como proyecto multipágina para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   base: '/tfg-visualizacion-sismica/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        station: resolve(__dirname, 'station.html'),
+      },
+    },
+  },
   
   server: {
     proxy: {
@@ -23,7 +32,7 @@ export default defineConfig({
         target: 'ws://live.openseismometer.net:18000', // ws://seedlink.openseismometer.net:18000 un subdominio para esto y otro para la web
         changeOrigin: true,
         secure: false, // Conectando a un endpoint ws:// no seguro
-        ws: true, // ⭐ CLAVE: Habilitar el proxy para WebSockets
+        ws: true, // CLAVE: Habilitar el proxy para WebSockets
         // La ruta /datalink-ws se reescribirá al path real del servicio DataLink (/datalink)
         rewrite: (path) => path.replace(/^\/datalink-ws/, '/datalink'),
       },
